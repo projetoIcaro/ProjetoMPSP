3620aefd0cac8c3a39fa7822ea550d35bc7cb046
Your Name <foo@example.com>
2019-08-17 15:10:46 -0300

    Mais Features SideBar

--- src/routes/Component/Sidebar/index.js
+++ src/routes/Component/Sidebar/index.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
 
 const SidebarExampleVisible = () => (
   <Sidebar.Pushable as={Segment}>
     <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
-      <Menu.Item as='a'>
-        <Icon name='home' />
-        Home
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='clock' />
-        History
-      </Menu.Item>
+      <Link to="/info/index">
+        <Menu.Item as='a'>
+          <Icon name='home' />
+          Home
+        </Menu.Item>
+      </Link>
+      <Link to="/history/index">
+        <Menu.Item as='a'>
+          <Icon name='clock' />
+          History
+        </Menu.Item>
+      </Link>
+      <Link to="/search/index">
+        <Menu.Item as='a'>
+          <Icon name='search' />
+          Search
+        </Menu.Item>
+      </Link>
     </Sidebar>
-
+    
     <Sidebar.Pusher>
       <Segment basic>
         <Header as='h3'>Application Content</Header>
