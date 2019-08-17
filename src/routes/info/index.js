7e736bc8cf09b06d9647c6276df0873d54675f5d
Your Name <foo@example.com>
2019-08-17 13:38:22 -0300

    Jara - Info Menu

--- src/routes/info/index.js
+++ src/routes/info/index.js
@@ -1,6 +1,11 @@
-import React, { Fragment } from 'react';
-import { Link } from "react-router-dom";
-import axios from 'axios';
+import React, { Fragment } from 'react'
+import { Link } from "react-router-dom"
+import { Image, Button, Container, Grid, Menu, Icon, Input, Segment } from 'semantic-ui-react'
+import axios from 'axios'
+import compassIcon from './resources/compassMenor.png'
+import menuButton from './resources/menu-button.png'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
 
 async function hardcodedSearch  () {
   await axios.get('http://localhost:3001/api/site/search', {
@@ -14,8 +19,31 @@ function index () {
   hardcodedSearch();
   return (
     <Fragment>
-      <div>teste</div>
-      <Link to={'/info/index'}>USERS/INDEX</Link>
+      <div id="Header" class="ui top attached menu" style={{marginTop:'20px'}}>
+        <div class="column">
+          <h2 class="ui header">
+            <img src ={compassIcon} style={{padding: 0}} /> 
+            <div class="content" style={{color: 'red'}}>
+              COMPASS
+            </div>
+          </h2>
+        </div>
+        <div id="search" class="ui search item">
+          <input type="text" class="Retngulo-7" placeholder="Search ..."></input>
+        </div>
+        <div class="ui right aligned category search item">
+          <div class="right column">
+            <div class="ui simple dropdown item">
+              More
+              <img scr={menuButton} />
+              <div class="menu">
+                <a class="item"><i class="settings icon"></i> Settings</a>
+                <a class="item"><i class="logout icon"></i> Logout</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
