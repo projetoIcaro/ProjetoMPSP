7e3e2ae9032460d5db064600b4e1608d07fd88af
fialhoFabio <fabiom456@gmail.com>
2019-08-17 14:59:35 -0300

    cards

--- src/routes/info/Component/Card/Card.js
+++ src/routes/info/Component/Card/Card.js
@@ -1,27 +1,32 @@
 import './Cards.css';
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import axios from 'axios';
 
-async function hardcodedSearch  (properties, apiName) {
-	await axios.get('http://localhost:3001/api/DataExtraction/' + apiName, {
-		params: {
-			...properties,
-		}
-	}).then(res => console.log(res))
-}
+import React, { useEffect, useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { apiExtractor } from '../../../../axiosconfig';
 
 function Card (props) {
 	const { apiName, properties } = props;
-	hardcodedSearch(properties, apiName);
+	const [data, setData] = useState({});
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		apiExtractor.get(apiName, {
+			params: { ...properties }
+		})
+			.then( res => setData(res) )
+			.finally(() => setLoading(false));
+	}, []);
 	return (
 		<div className="ui card">
 			<div className="content">
 				<div className="header">{apiName}</div>
 				<div className="description ui segment">
-					<div className="ui active dimmer">
-						<div className="ui text loader">Loading</div>
-					</div>
+					{loading ? (
+						<div className="ui active dimmer">
+							<div className="ui text loader">Loading</div>
+						</div>
+					) : (
+						<div>INFORMACOES LEGAIS</div>
+					)}
 				</div>
 			</div>
 		</div>
