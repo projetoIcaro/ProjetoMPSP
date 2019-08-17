ac0409fc4f3747ab5ccc4ea243959e9430839c22
samantharie <samantha.rie@gmail.com>
2019-08-17 16:53:46 -0300

    login page done

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
-import { Image, Button, Container, Grid, Menu, Icon, Input, Segment, Item, Label, ItemHeader } from 'semantic-ui-react';
+import { Image, Button, Container, Grid, Menu, Icon, Input, Segment, Item, Label, Modal, Form, Header } from 'semantic-ui-react';
 import bolinhas from './bolinhas.png';
 import 'semantic-ui-css/semantic.min.css';
 import RightMenu from '../Component/RightMenu/index'
@@ -30,8 +30,52 @@ function index () {
   </Grid>
       
   <Container> 
-    <Grid>
-    
+    <Grid style={{marginLeft:350}}>
+      <Segment>
+        <Form>
+          <Header color='red'><h2>LOG IN</h2></Header>
+          <Form.Field>
+            <Input icon placeholder='E-mail' size='huge'>
+              <input />
+              <Icon name='envelope'/>
+            </Input>
+          </Form.Field>
+          <Form.Field>
+            <Input icon placeholder='Senha' size='huge'>
+              <input />
+              <Icon name='lock'/>
+            </Input>
+          </Form.Field>
+        </Form>
+
+        <Modal trigger={<Item.Group link>
+          <Item>
+            <Item.Header><h5>Esqueci a senha</h5></Item.Header>
+          </Item>   
+        </Item.Group>}>
+          <Modal.Header>Recuperação de Senha</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Header>Digite seu e-mail:</Header>
+              <Input icon placeholder='E-mail' size='medium' fluid>
+                <input />
+                <Icon name='envelope'/>
+              </Input>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='red' inverted>
+              <Icon name='remove' /> Cancelar
+            </Button>
+            <Button color='green' inverted>
+              <Icon name='checkmark' /> Enviar
+            </Button>
+          </Modal.Actions>
+        </Modal>
+
+        <Button circular color='red' floated='right' href="/search/index">LOG IN</Button> 
+      </Segment>
+      
     </Grid>
   </Container>
 </Fragment> 
