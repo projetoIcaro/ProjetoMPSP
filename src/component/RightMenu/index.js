4b7f0972d225cd02412ca64d1c33c5bc0f5d29c6
fialhoFabio <fabiom456@gmail.com>
2019-09-05 02:51:41 -0300

    code fixes

--- src/component/RightMenu/index.js
+++ src/component/RightMenu/index.js
@@ -8,8 +8,8 @@ export default class RightMenu extends Component {
         <div className="ui simple dropdown item">
             <Icon name='bars' size='big'/>
             <div className = "menu">
-                <Link to="/user/settings/index"><a className = "item"><i className = "settings icon"/> Settings</a> </Link>
-                <Link to="/login/index"> <a href='/' className = "item"><i className = "logout icon"/> Logout</a> </Link>
+                <Link to="/user/settings/index"><i className = "settings icon"/> Settings</Link>
+                <Link to="/login/index"><i className = "logout icon"/> Logout</Link>
             </div>
         </div>
         );
--- src/routes/user/settings/index.js
+++ src/routes/user/settings/index.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment} from 'react';
-import { Button, Container, Grid, Menu, Icon, Input, Segment, Item, Modal, Form, Header } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 export default class settings extends Component {
