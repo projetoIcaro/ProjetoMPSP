f43da462948aab4eb697ec1ed506d99fc9f13b59
fialhoFabio <fabiom456@gmail.com>
2019-09-17 19:13:27 -0300

    menu

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,7 +1,7 @@
 import style from './index.module.css';
 import 'semantic-ui-css/semantic.min.css'
 
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Header as HeaderSemantic, Input, Menu} from 'semantic-ui-react'
 import RightMenu from 'component/RightMenu';
 import compassIcon from './images/compassMenor.png'
@@ -9,28 +9,15 @@ import compassMenu from './images/compass-menu.png'
 
 function Header (props) {
 	return (
-		<div className={style.wrapper}>
-			{props.isLogin ? (
-				<img alt="compass" src={compassMenu}/>
+		<div className = {style.wrapper}>
+			{!props.isLogin ? (
+				<img alt="compass" src = {compassMenu}/>
 			) : (
-				<Menu>
-					<HeaderSemantic>
-						<h2 className="ui header">
-							<img alt="compassIcon" src={compassIcon}
-									 style={{padding: 0, marginTop: 20, marginLeft: 15, width: 55, height: 50}}/>
-							<div className="content" style={{color: 'red', marginTop: 20}}>
-								<h3>COMPASS</h3>
-							</div>
-						</h2>
-					</HeaderSemantic>
-					<Menu.Item>
-						<Input icon='search' placeholder='Search...'/>
-					</Menu.Item>
-					<Menu.Item position='right'>
-						<RightMenu/>
-					</Menu.Item>
-				
-				</Menu>
+				<div>
+					<img alt="compass" src = {compassMenu}/>
+					<p>INPUT</p>
+					<p>TROSSO DO USUARIO</p>
+				</div>
 			)}
 		</div>
 	);
