269db99caa2560b65030166d65ad719fe566dee7
Gabriel Martinho Jara <gabrielmjara99@gmail.com>
2019-10-09 02:33:58 -0300

    PDF Viewer working

--- src/routes/investigation/download/Download.js
+++ src/routes/investigation/download/Download.js
@@ -1,62 +1,55 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {isImmutable} from 'immutable';
 import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 
 
 function DownloadButton (props) {
 
     const MyDocument = GerarPDF(props.extractionData.toJS())
-    
+
     return (
-        <PDFViewer>
+        <PDFViewer width={'100%'}>
             {MyDocument}
         </PDFViewer>
     )
 }
 
 const GerarPDF = (data = null, isPreview = false) => {
-    const dataToRender = [];
 
     const styles = StyleSheet.create({
         page: {
-          flexDirection: 'row',
-          backgroundColor: '#E4E4E4'
+          flexDirection: 'column',
+          backgroundColor: 'white',
+          fontFamily: 'Helvetica',
         },
-        section: {
-          margin: 10,
-          padding: 10,
-          flexGrow: 1
+        sectionRow: {
+          margin: 1,
+          padding: 1,
+          flexGrow: 1,
+          color: 'black',
+          textAlign: 'center'
         }
       });
 
-    const MyDocument = (
+    const MyDocument = (       
         <Document>
             <Page size="A4" style={styles.page}>
-                <View style={styles.section}>
+                <View style={styles.sectionRow}>
                     <Text>Section #1</Text>
                 </View>
-                <View style={styles.section}>
-                    <Text>Section #2</Text>
+            </Page>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.sectionRow}>
+                    <Text>Section #1</Text>
+                </View>
+            </Page>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.sectionRow}>
+                    <Text>Section #1</Text>
                 </View>
             </Page>
         </Document>
     );
-
-    for (var api in data) {
-        console.log(`${api}`)
-        for (var resultado1 in data[api]) {
-            if(typeof data[api][resultado1] == 'object'){
-                console.log(`${resultado1} :`)
-                for (var resultado2 in data[api][resultado1]) {
-                    console.log(`       ${resultado2} e ${data[api][resultado1][resultado2]}`)    
-                }
-            }
-            else{
-                console.log(`   ${resultado1} e ${data[api][resultado1]}`)
-            }
-        }
-    }
 
     for (var api in data) {
         console.log(`${api}`)
